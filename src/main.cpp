09e93726d24df426ee0e56cd6f34fa8228a96566
israel22310169 <159966207+israel22310169@users.noreply.github.com>
2024-04-17 00:23:08 +0000

    version 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include <string>
+#include <thread>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    std::cout<<"space invaders"std
+    Element lienzo = hbox({
+        spinner(21,4) | bold
+    });
+
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Fit(lienzo)
+        );
+
+        Render(pantalla,lienzo);
+        pantalla.Print();
+        pantalla.ResetPosition();
+        this_thread::sleep_for(0.1s);
+        
+
+
+
     return 0;
 }
--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,22 +1,24 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
-#include <ftxui/screen/string.hpp>
-
-using namespace ftxui;
-
-int main() {
-
-    //crea los elementos de la interfaz de usuario 
-    Element label = text("Hola, mundo!");
-    Element container = hbox({label});
-
-    //crea la pantalla y establece el contenido
-    auto screen = Screen::Create(
-        Dimension::Full(),
-        Dimension::Fit(container)
-    );
-   Render (screen,container);
-   screen.Print();
-   
-   return 0;
+#include <iostream>
+ 
+int main(void) {
+  using namespace ftxui;
+ 
+  // Define the document
+  Element document =
+    hbox({
+      text("left")   | border,
+      text("middle") | border | flex,
+      text("right")  | border,
+    });
+ 
+  auto screen = Screen::Create(
+    Dimension::Full(),       // Width
+    Dimension::Fit(document) // Height
+  );
+  Render(screen, document);
+  screen.Print();
+ 
+  return EXIT_SUCCESS;
 }
